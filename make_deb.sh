d63da27ed76c4a390fe9bb953998a2d1d29fc8a7
Simon Schneegans <simon.schneegans@uni-weimar.de>
2011-08-12 22:07:53 +0200

    improved deb build

--- make_deb.sh
+++ make_deb.sh
@@ -8,8 +8,20 @@ mkdir debian/tmp/DEBIAN
 
 cp debian/control debian/tmp/DEBIAN/control
 
-dpkg -b "debian/tmp" 
+
+replace='s/ARCH_REPLACE/i368/'
+
+# Figure out user's machine architecture and launch correct executable
+MACHINE=`uname -m`
+if [ "$MACHINE" = "x86_64" ]; then
+    replace='s/ARCH_REPLACE/amd64/'
+fi
+
+sed --in-place debian/tmp/DEBIAN/control --expression=$replace
+
+fakeroot dpkg -b "debian/tmp" 
 
 rm -r debian/tmp 
 
 mv debian/tmp.deb gnome-pie.deb
+

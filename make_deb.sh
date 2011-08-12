fa8c6d22e576a12c46396690552e9161b4f40f18
Simon Schneegans <simon.schneegans@uni-weimar.de>
2011-08-12 22:13:11 +0200

    improved deb build

--- make_deb.sh
+++ make_deb.sh
@@ -2,7 +2,13 @@
 
 mkdir build
 
-cd build; cmake -DCMAKE_INSTALL_PREFIX=../debian/tmp/usr ..; make install && cd ..;
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=../debian/tmp/usr ..
+
+make install
+
+cd ..
 
 mkdir debian/tmp/DEBIAN
 
@@ -11,7 +17,7 @@ cp debian/control debian/tmp/DEBIAN/control
 
 replace='s/ARCH_REPLACE/i368/'
 
-# Figure out user's machine architecture and launch correct executable
+# Figure out user's machine architecture and put it in the control file
 MACHINE=`uname -m`
 if [ "$MACHINE" = "x86_64" ]; then
     replace='s/ARCH_REPLACE/amd64/'
@@ -19,6 +25,7 @@ fi
 
 sed --in-place debian/tmp/DEBIAN/control --expression=$replace
 
+# build the package
 fakeroot dpkg -b "debian/tmp" 
 
 rm -r debian/tmp 
